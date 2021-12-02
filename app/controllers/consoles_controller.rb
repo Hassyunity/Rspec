950087dce59e7561a3fd8fccbb2064ac114f03fb
HassyUnity <hassy.tsihoarana@gmail.com>
2021-12-02 11:07:42 +0300

    add test mapping in requests spec

--- app/controllers/consoles_controller.rb
+++ app/controllers/consoles_controller.rb
@@ -1,25 +1,24 @@
 class ConsolesController < ApplicationController
     def index
         consoles = [
-        #    { name:'NES', manufacturer: 'Nintendo' },
-        #    { name:'Wii', manufacturer: 'Nintendo' },
-        #    { name:'SNES', manufacturer: 'Nintendo' },
-        #    { name:'Genesis', manufacturer: 'Nintendo' },
-        #    { name:'Switch', manufacturer: 'Nintendo' },
-        #    { name:'PS1', manufacturer: 'Nintendo' },
-        #    { name:'PS5', manufacturer: 'Nintendo' },
-        #    { name:'NES', manufacturer: 'Nintendo' },
+           { name:'NES', manufacturer: 'Nintendo' },
+           { name:'SNES', manufacturer: 'Nintendo' },
+           { name:'Wii', manufacturer: 'Nintendo' },
+           { name:'Genesis', manufacturer: 'Nintendo' },
+           { name:'Switch', manufacturer: 'Nintendo' },
+           { name:'PS1', manufacturer: 'Nintendo' },
+           { name:'PS5', manufacturer: 'Nintendo' }
 
-                'NES', 
-                'SNES',
-                'Wii',
-                'Genesis',
-                'Switch',
-                'PS1',
-                'PS5'
+                # 'NES', 
+                # 'SNES',
+                # 'Wii',
+                # 'Genesis',
+                # 'Switch',
+                # 'PS1',
+                # 'PS5'
             
         ]
-        render(json: { 'consoles' => consoles })
-        # render(json: { 'consoles' => consoles.map { |console| console[:name]} })
+        # render(json: { 'consoles' => consoles })
+        render(json: { 'consoles' => consoles.map { |console| console[:name]} })
     end 
 end
